9548a512f0a9b9f79bc4f8f3f65414fb5cb5794a
David Starr <dstarrtrey@gmail.com>
2019-01-04 14:42:42 -0800

    changes colors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     $(".nav-link").hover(function() {
-            $(`#${this.id}-ttl`).addClass( "hover" ); 
+            $(`#${this.id}-ttl`).removeClass( "text-off" );
+            $(`#${this.id}-img`).removeClass( "img-off" );
+            $(`#${this.id}-solid`).removeClass( "solid-off" );
+            $(`#${this.id}-ttl`).addClass( "text-hover" ); 
             $(`#${this.id}-img`).addClass( "img-hover" );  
             $(`#${this.id}-solid`).addClass( "solid-hover" ); 
         }, function() {
-            $(`#${this.id}-ttl`).removeClass( "hover" );
+            $(`#${this.id}-ttl`).addClass( "text-off" );
+            $(`#${this.id}-img`).addClass( "img-off" );
+            $(`#${this.id}-solid`).addClass( "solid-off" );
+            $(`#${this.id}-ttl`).removeClass( "text-hover" );
             $(`#${this.id}-img`).removeClass( "img-hover" );
             $(`#${this.id}-solid`).removeClass( "solid-hover" );
         }
