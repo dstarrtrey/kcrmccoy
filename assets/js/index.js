fef802b552bd32c7d108cf8c6917d1c4e4cbcd4b
David Starr <dstarrtrey@gmail.com>
2019-01-04 12:58:18 -0800

    modifies header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     $(".nav-link").hover(function() {
-            $(`#${this.id}-ttl`).addClass( "hover" );   
+            $(`#${this.id}-ttl`).addClass( "hover" ); 
+            $(`#${this.id}-img`).addClass( "img-hover" );  
+            $(`#${this.id}-solid`).addClass( "solid-hover" ); 
         }, function() {
             $(`#${this.id}-ttl`).removeClass( "hover" );
+            $(`#${this.id}-img`).removeClass( "img-hover" );
+            $(`#${this.id}-solid`).removeClass( "solid-hover" );
         }
     );
 });
