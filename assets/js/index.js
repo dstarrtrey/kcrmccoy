013281679fc7f7f37792f3d07dc12ec0d6cc6fdb
David Starr <dstarrtrey@gmail.com>
2019-01-11 23:40:26 -0800

    edits spin animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -93,12 +93,12 @@ $(document).ready(function(){
             animateOut($(`#${link.target.id}`));
             
             setTimeout(function(){
-                triangles.addClass("spin");
+                main.addClass("spin");
                 setTimeout(function(){
                     requestContent(url);
                     history.pushState(data, null, url);
                     animateIn($(`#${link.target.id}`));
-                    triangles.removeClass("spin");
+                    main.removeClass("spin");
                 }, 2500)
             }, animationLength);
         }
