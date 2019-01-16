f1c9a2a7f693c2a074780041f1345eb7429b25af
David Starr <dstarrtrey@gmail.com>
2019-01-16 11:39:57 -0800

    modifies lego slideshow

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,7 +74,7 @@ $(document).ready(function(){
         //fixes width of content
     }
     const requestContent = (file, data) => {
-        all.load(`${file} .all`, function(){
+        main.load(`${file} .all`, function(){
             if(data==="lego"){
                 console.log("executing:");
                 let photos = [];
